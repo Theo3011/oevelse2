5aad94d4565388cf17e74899376b2dd3d9b60c7e
CCOvergaard <cach22ad@student.cbs.dk>
2024-09-13 12:31:12 +0200

    prop
    propcomponent

--- ComponentProject/App.js
+++ ComponentProject/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import FirstComponent from "./components/FirstComponent"; 
+import PropsComponent from "./components/PropsComponent";  // Sørg for, at stien til PropsComponent er korrekt
 
 export default function App() {
   return (
     <View style={styles.container}>
+      {/* Første komponent */}
       <View
         style={{
           flex: 1,
@@ -16,6 +18,19 @@ export default function App() {
         }}
       >
         <FirstComponent />
+      </View>
+
+      {/* Nyt view med PropsComponent */}
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: "lightgreen",
+          alignItems: "center",
+          justifyContent: "center",
+          width: "100%",
+        }}
+      >
+        <PropsComponent name="Dit-navn-her" />
       </View>
 
       <StatusBar style="auto" />
---
+++ ComponentProject/components/PropsComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const PropsComponent = ({ name }) => {
+    return (
+        // Opret en View og Text, der viser "Made by {name}"
+        <View>
+            <Text>Made by {name}</Text>
+        </View>
+    )
+}
+
+export default PropsComponent;
