970687f47434700bfa99203bda7246e0dbce7947
Theo3011 <44906882+Theo3011@users.noreply.github.com>
2024-09-13 12:44:44 +0200

    s
    s

--- ComponentProject/App.js
+++ ComponentProject/App.js
@@ -2,41 +2,27 @@ import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import FirstComponent from "./components/FirstComponent"; 
-import PropsComponent from "./components/PropsComponent";  // Sørg for, at stien til PropsComponent er korrekt
+import PropsComponent from "./components/PropsComponent"; 
+
 
 export default function App() {
   return (
     <View style={styles.container}>
       {/* Første komponent */}
-      <View
-        style={{
-          flex: 1,
-          backgroundColor: "lightblue",
-          alignItems: "center",
-          justifyContent: "center",
-          width: "100%",
-        }}
-      >
+      <View style={styles.firstComponentContainer}>
         <FirstComponent />
       </View>
 
       {/* Nyt view med PropsComponent */}
-      <View
-        style={{
-          flex: 1,
-          backgroundColor: "lightgreen",
-          alignItems: "center",
-          justifyContent: "center",
-          width: "100%",
-        }}
-      >
-        <PropsComponent name="Dit-navn-her" />
+      <View style={styles.propsComponentContainer}>
+        <PropsComponent name="nigga" />
       </View>
 
       <StatusBar style="auto" />
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -45,5 +31,19 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     paddingTop: 50,
+  },
+  firstComponentContainer: {
+    flex: 1,
+    backgroundColor: "lightblue",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+  },
+  propsComponentContainer: {
+    flex: 1,
+    backgroundColor: "lightgreen",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
   },
 });
